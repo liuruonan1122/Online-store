c7f70e03704dfb301e6a542ee4b7c5a28a31c46a
KK525666 <1790758908@qq.com>
2025-12-15 16:45:02 +0800

    Add files via upload

---
+++ 购物车模块/后端/mapper/CartMapper.java
@@ -0,0 +1,14 @@
+package com.kk.em.mapper;
+
+import com.kk.em.entity.Cart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.MapKey;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CartMapper extends BaseMapper<Cart> {
+
+    @MapKey("id")
+    List<Map<String, Object>> selectByUserId(Long userId);
+}
