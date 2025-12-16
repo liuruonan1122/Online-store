fcd697423e72285cab2c9ceab66668d47db012db
luo613 <2158327988@qq.com>
2025-12-16 17:32:46 +0800

    order

---
+++ order/backend/mapper/OrderGoodsMapper.java
@@ -0,0 +1,8 @@
+package com.kk.em.mapper;
+
+import com.kk.em.entity.OrderGoods;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface OrderGoodsMapper extends BaseMapper<OrderGoods> {
+
+}
